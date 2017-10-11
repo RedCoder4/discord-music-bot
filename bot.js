44d9e2f2a1e45a03525efe07aa23bc7b80bff03c
RedCoder4 <bd5@hotmail.com.au>
2017-10-12 10:34:32 +1100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,8 @@
+var bot = require("discord-music-bot");
+
+var serverName = "FAVA Radio";
+var textChannelName = "bot";
+var voiceChannelName = "Radio";
+var botToken = "MzY2ODM4ODAyNjY2MjI1NjY1.DLzYQA.NAvhXUcRXzsOPJ0u6Qvuunkcv1Y";
+
+bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
