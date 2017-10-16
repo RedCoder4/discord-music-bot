a4aa83ab1199bedc48f92cfb148fa4df3e4103d0
RedCoder4 <bd5@hotmail.com.au>
2017-10-16 20:50:42 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,3 +6,5 @@ var voiceChannelName = "Radio";
 var botToken = "MzY2ODM4ODAyNjY2MjI1NjY1.DLzYQA.NAvhXUcRXzsOPJ0u6Qvuunkcv1Y";
 
 bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
+
+client.login(process.env.BOT_TOKEN);
